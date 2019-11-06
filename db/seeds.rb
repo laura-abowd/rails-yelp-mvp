805bd6d20b0b42f2d7fd3a45746422e7ab62f664
Laura Abowd <laura.abowd@gmail.com>
2019-11-06 17:19:01 +0900

    added styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Mcdonalds = Restaurant.create(name: 'McDonalds', address: 'Tokyo', phone_number: 123456, category: 'chinese')
+
+TacoBell = Restaurant.create(name: 'Taco Bell', address: 'Shibuya', phone_number: 345321, category: 'italian')
+
+OliveGarden = Restaurant.create(name: 'Olive Garden', address: 'NakaMeguro', phone_number: 432567, category: 'belgian')
+
+Sushi = Restaurant.create(name: 'Sushi', address: 'adachi', phone_number: 478281, category: 'japanese' )
+
+Sukiya = Restaurant.create(name: 'Sukiya', address: 'Shinjuku', phone_number: 123899, category: 'french')
